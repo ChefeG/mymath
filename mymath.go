19f41fa5e0d0e8587600823ce3a163c7d3ae2532
ChefeG <86253972+ChefeG@users.noreply.github.com>
2023-12-26 21:39:27 +0300

    Update mymath.go
    some edits to Max and Yn

--- mymath.go
+++ mymath.go
@@ -4,18 +4,15 @@ import "math"
 
 // Sqrt - функция для вычисления квадратного корня из числа
 func Abs(x float64) float64 {
-	return math.Sqrt(x)
+	return math.Abs(x)
 }
 
-func Max(x float64) float64 {
-	return math.Sqrt(x)
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
 }
 
 func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
 }
 
-func Yn(x float64) float64 {
-	return math.Sqrt(x)
-}
-
+func Yn(x int, y float64) float64 { return math.Yn(x, y) }
