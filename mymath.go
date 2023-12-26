38523f3ab7a1ca647a26be3de8a5263772d5a865
ChefeG <86253972+ChefeG@users.noreply.github.com>
2023-12-26 16:37:52 +0300

    Add files via upload

--- mymath.go
+++ mymath.go
@@ -3,6 +3,19 @@ package mymath
 import "math"
 
 // Sqrt - функция для вычисления квадратного корня из числа
+func Abs(x float64) float64 {
+	return math.Sqrt(x)
+}
+
+func Max(x float64) float64 {
+	return math.Sqrt(x)
+}
+
 func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
 }
+
+func Yn(x float64) float64 {
+	return math.Sqrt(x)
+}
+
